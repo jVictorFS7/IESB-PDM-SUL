0f22189160284011ebb8d45156c09c94ec2ebd59
unknown <2514290045@ACADEMICO.IESB.BR>
2026-08-19 21:23:18 -0300

    subindo minha atividade prática03

--- praticas/pratica03/app-atividade-aula-tarefas/App.js
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -5,23 +5,36 @@ import { rotulo_btn_cadastro_meta, rotulo_lista_metas, rotulo_input_meta } from 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TextInput placeholder={rotulo_input_meta}/>
-      <Button title={rotulo_btn_cadastro_meta}/>
-      <Text>{rotulo_lista_metas}</Text>
-      <TextInput placeholder='Digite uma tarefa...'></TextInput>
-      <StatusBar style="auto" />
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', flex: 1}}>
+        <View style={{ width: '65%' }}>
+          <TextInput style={styles.inputText} placeholder={rotulo_input_meta} />
+        </View>
+        <View style={{ width: '30%' }}>
+          <Button title={rotulo_btn_cadastro_meta} />
+        </View>
+      </View>
+      <View style={styles.metaContainer}>
+        <Text>{rotulo_lista_metas}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   mainConteiner: {
     padding: 30,
+    flex: 1,
+    flexDirection: "column",
+  },
+  inputText: {
+    borderColor: "#cccccc",
+    borderWidth: 1
+  },
+  metaContainer: {
+    flex: 1
   }
 });
